cf585931578272c539f01efec8d4c832aacaf72d
dolphius21 <johnrudolphlee@gmail.com>
2020-07-27 15:09:48 +0800

    added customers.json

--- Ajax_&_JSON/app.js
+++ Ajax_&_JSON/app.js
@@ -1,4 +1,4 @@
-const loadCustomer = (e) => {
+const loadCustomer = () => {
   const xhr = new XMLHttpRequest();
 
   xhr.open('GET', 'customer.json', true);
@@ -20,6 +20,37 @@ const loadCustomer = (e) => {
     }
   };
   xhr.send();
-}
+};
+
+const loadCustomers = () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const customers = JSON.parse(xhr.responseText);
+      let output = '';
+      
+      customers.forEach(customer => {
+        output += `
+          <ul>
+            <li>ID: ${customer.id}</li>
+            <li>Name: ${customer.name}</li>
+            <li>Company: ${customer.company}</li>
+            <li>Phone: ${customer.phone}</li>
+          </ul>
+        `;
+      });
+
+      document.getElementById('customers').innerHTML = output;
+
+    } else {
+      console.log('Error parsing data...')
+    }
+  };
+  xhr.send();
+};
 
 document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
